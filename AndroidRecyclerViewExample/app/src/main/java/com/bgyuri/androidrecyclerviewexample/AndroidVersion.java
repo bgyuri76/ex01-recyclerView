a5ae36faa500eba3d99e5443ad04bd5063b7f86d
GyuriGB <bgyuridev@gmail.com>
2016-07-26 00:59:37 +0300

    working app

---
+++ AndroidRecyclerViewExample/app/src/main/java/com/bgyuri/androidrecyclerviewexample/AndroidVersion.java
@@ -0,0 +1,25 @@
+package com.bgyuri.androidrecyclerviewexample;
+
+/**
+ * Created by GyuriGB on 2016.07.26..
+ */
+public class AndroidVersion {
+    private String recyclerViewTitleText;
+    private int recyclerViewImage;
+
+    public String getrecyclerViewTitleText() {
+        return recyclerViewTitleText;
+    }
+
+    public void setAndroidVersionName(String recyclerVietTitleText) {
+        this.recyclerViewTitleText = recyclerVietTitleText;
+    }
+
+    public int getrecyclerViewImage() {
+        return recyclerViewImage;
+    }
+
+    public void setrecyclerViewImage(int recyclerViewImage) {
+        this.recyclerViewImage = recyclerViewImage;
+    }
+}
